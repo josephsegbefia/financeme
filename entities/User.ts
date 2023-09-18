2599cb7c6978f61a9f8363ec9e0832b1f19a8241
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-09-19 01:06:33 +0200

    Add getter methods

--- entities/User.ts
+++ entities/User.ts
@@ -1,14 +1,23 @@
 class User {
-  name: string;
-  age: number;
+  private name: string;
+  private age: number;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
   printUserInfo() {
     console.log(`name: ${this.name}, age: ${this.age}`);
   }
 }
+
+// const user1 = new User("Joseph", 10);
+// console.log(user1.getName());
 export default User;
